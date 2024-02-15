d92f1656ede506a6f9255f91ab354d8cb2e12a48
topherwalker <git@christopherwalker.tech>
2024-02-15 15:38:27 +0000

    Doc: documented roman numeral conversion

--- src/Roman-Numeral.test.ts
+++ src/Roman-Numeral.test.ts
@@ -1,4 +1,11 @@
 import { romanNumeral } from "./Roman-Numerals";
+// I = 1
+// V = 5
+// X = 10
+// L = 50
+// C = 100
+// D = 500
+// M = 1000
 describe("", () => {
   it("", () => {
     expect("").toBe("");
--- src/Roman-Numerals.ts
+++ src/Roman-Numerals.ts
@@ -1,3 +1,11 @@
+// I = 1
+// V = 5
+// X = 10
+// L = 50
+// C = 100
+// D = 500
+// M = 1000
+
 export function romanNumeral(number: number): string {
   return "";
 }
